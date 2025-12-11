d8e77c1c06a5c2d1ec5ea41f8dabbd5625ed3472
Erik Bahena <erikjbahena@gmail.com>
2025-12-11 09:50:13 -0800

    v1.0.10

--- src/index.js
+++ src/index.js
@@ -325,7 +325,7 @@ toMp4.TSParser = TSParser;
 toMp4.RemoteMp4 = RemoteMp4;
 
 // Version (injected at build time for dist, read from package.json for ESM)
-toMp4.version = '1.0.9';
+toMp4.version = '1.0.10';
 
 // Export
 export { 
